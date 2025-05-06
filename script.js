2822573c371acc5898b065a450b852d455fc7d3b
Christian Buco <christianbuco17@gmail.com>
2025-05-05 17:54:00 -0700

    add scripts

---
+++ script.js
@@ -0,0 +1,36 @@
+async function verifyEmail() {
+  const statusEl = document.getElementById('status');
+  const params = new URLSearchParams(window.location.search);
+  const token = params.get('token');
+
+  if (!token) {
+    statusEl.textContent = "Invalid verification link.";
+  } else {
+    try {
+      const res = await fetch(` https://sacstate-backend.azurewebsites.net/api/signup/verify?token=${token}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!res.ok) {
+        throw new Error("Verification failed");
+      }
+
+      const data = await res.json();
+      console.log(data);
+      statusEl.textContent = data.message || "Email successfully verified!";
+
+    } catch (err) {
+      console.error(err);
+      statusEl.textContent = "Verification failed or link expired.";
+    }
+  }
+}
+
+window.addEventListener('load', () => {
+  verifyEmail();
+});
+
+module.exports = { verifyEmail };
